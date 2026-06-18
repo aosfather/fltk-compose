01f7e43b486167a558663e8737b8eb49454ff526
aosfather <aosfather@gmail.com>
2026-06-18 22:48:41 +0800

    stringtable可用

--- compose/table.go
+++ compose/table.go
@@ -35,33 +35,76 @@ type Cell struct {
 	Data     CellData
 	RawValue string
 }
+
+type _Model_Row []string
 type TableModel struct {
-	Cells [][]*Cell
+	Cells    [][]*Cell
+	colCount int
+	cols     []string
+	rows     []_Model_Row
+}
+
+func (m *TableModel) Count() int {
+	return len(m.rows)
+}
+
+func (m *TableModel) AddRow(row ...string) {
+	r := make(_Model_Row, m.colCount)
+	copy(r, row)
+	m.rows = append(m.rows, r)
+}
+
+func (m *TableModel) Cell(r, c int) string {
+	if c >= m.colCount || r < 0 || r >= m.Count() {
+		return ""
+	}
+
+	return m.rows[r][c]
+}
+
+type TableView interface {
+	AddRow(row ...string)
 }
 
 type _Table struct {
 	Rect
 	_Component
-	wrap     *fltk.TableRow
-	rowCount int
-	colCount int
-	model    TableModel
+	wrap  *fltk.TableRow
+	model TableModel
 }
 
+func (t *_Table) SetOptions(cols []string) {
+	t.model.cols = cols
+	t.model.colCount = len(t.model.cols)
+}
 func (t *_Table) _Render() {
 	t.applyModifier()
-	t.rowCount = 10
-	t.colCount = 3
 	if t.wrap == nil {
 		t.wrap = fltk.NewTableRow(t.x, t.y, t.width, t.height)
-		t.wrap.SetRowCount(t.rowCount)
-		t.wrap.SetColumnCount(t.colCount)
+		t.wrap.SetRowCount(0)
+		t.wrap.SetColumnCount(t.model.colCount)
 		t.wrap.EnableColumnHeaders()
-		t.wrap.EnableRowHeaders()
+		// t.wrap.EnableRowHeaders()
 		t.wrap.AllowColumnResizing()
+		// t.wrap.SetColumnWidth(0, 20)
+		// t.wrap.SetColumnWidth(1, 20)
+		// t.wrap.SetColumnWidth(2, 180)
 		t.wrap.AllowRowResizing()
 		t.wrap.SetDrawCellCallback(t.drawCell)
 	}
+}
+func (t *_Table) AddRow(row ...string) {
+	t.model.AddRow(row...)
+	t.wrap.SetRowCount(t.model.Count())
+	// t.wrap.Redraw()
+}
+
+func (t *_Table) Bind(o *BindObj[TableView]) *_Table {
+	if o != nil {
+		o.getter = func() TableView { return t }
+		o.setter = func(TableView) {}
+	}
+	return t
 }
 
 func (t *_Table) drawCell(tc fltk.TableContext, i, j, x, y, w, h int) {
@@ -78,21 +121,21 @@ func (t *_Table) drawCell(tc fltk.TableContext, i, j, x, y, w, h int) {
 		fltk.SetDrawFont(fltk.HELVETICA_BOLD, 14)
 		fltk.DrawBox(fltk.UP_BOX, x, y, w, h, fltk.BACKGROUND_COLOR)
 		fltk.SetDrawColor(fltk.BLACK)
-		fltk.Draw(col.String(), x, y, w, h, fltk.ALIGN_CENTER)
+		fltk.Draw(t.model.cols[col], x, y, w, h, fltk.ALIGN_CENTER)
 	case fltk.ContextCell:
 		// loc := CellLoc{Row: row, Col: col}
 		fltk.SetDrawFont(fltk.HELVETICA, 14)
-		fltk.DrawBox(fltk.FLAT_BOX, x, y, w, h, fltk.BLACK)
+		fltk.DrawBox(fltk.FLAT_BOX, x, y, w, h, fltk.LIGHT1)
 		fltk.DrawBox(fltk.FLAT_BOX, x+1, y+1, w-2, h-2, fltk.WHITE)
 		fltk.SetDrawColor(fltk.BLACK)
-		if len(t.model.Cells) == 0 {
+		if t.model.Count() == 0 {
 			return
 		}
-		fltk.Draw(t.model.Cells[row][col].RawValue, x, y, w, h, fltk.ALIGN_CENTER)
+		fltk.Draw(t.model.rows[row][col], x, y, w, h, fltk.ALIGN_CENTER)
 	}
 }
 
-func Table(m ...Modifier) Component {
+func Table(m ...Modifier) *_Table {
 	t := &_Table{}
 	t.modifiers = m
 	t.self = t
--- example/backtest/back.go
+++ example/backtest/back.go
@@ -18,6 +18,7 @@ type Back struct {
 	project  compose.BindObj[string]
 	view     compose.BindObj[compose.Log]
 	progress compose.BindObj[float64]
+	tv       compose.BindObj[compose.TableView]
 }
 
 func (b *Back) Init() {
@@ -30,11 +31,12 @@ func (b *Back) Init() {
 		compose.Input(compose.IM_Normal, attr.M().Point(40, 40).Size(180, 25).M()...).Bind(&b.project),
 		compose.Button(attr.M().Point(200, 75).Size(80, 20).Title("开始回测").M()...).Event(b.startBackTest),
 		compose.LogView(attr.M().Point(10, 100).Size(280, 260).M()...).Bind(&b.view))
+
 	tabs.NewTab("盯盘",
 		compose.Progress(attr.M().Point(10, 20).Size(280, 20).Min(0).Max(100).M()...).Bind(&b.progress),
 		compose.Slider(attr.M().Point(10, 60).Size(200, 20).Max(16.6).M()...),
 		compose.Spinner(attr.M().Point(10, 90).Size(100, 25).M()...),
-		compose.Table(attr.M().Point(10, 120).Size(260, 200).M()...))
+		compose.Table(attr.M().Point(10, 120).Size(260, 200).Options("第一列", "第二列").M()...).Bind(&b.tv))
 	b.Layout(tabs)
 }
 
@@ -44,4 +46,5 @@ func (b *Back) startBackTest(sender compose.Component, data *compose.EventData) 
 	log.Warn("sell:%s", b.product.Get())
 	v := b.progress.Get()
 	b.progress.Set(v + 5)
+	b.tv.Get().AddRow("测试", b.product.Get())
 }
